bd8cc49b6572f21d67ead206f443eefb669c9fbd
John Scigulinsky <john.scigulinsky@gmail.com>
2019-05-16 14:19:36 +0200

    final

--- Reflexion/Program.cs
+++ Reflexion/Program.cs
@@ -9,7 +9,21 @@ namespace Reflexion
     class Program
     {
         static void Main(string[] args)
-        {   
+        {
+            MojaTrieda tr = new MojaTrieda();
+
+
+            Type t = tr.GetType();
+            var prop = t.GetProperties();
+            prop[0].SetValue(tr, 500);
+            prop[1].SetValue(tr, "ajpj");
         }
     }
+
+    public class MojaTrieda
+    {
+        public int MyProperty { get; set; }
+        public string MyProperty2 { get; set; }
+    }
+
 }
